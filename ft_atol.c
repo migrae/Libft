e52bd590643b4b90b1097fa2ce3c5ef00ce4aa4d
Michael <116090305+migrae@users.noreply.github.com>
2023-02-03 09:14:10 +0100

    Added ft_atol

---
+++ ft_atol.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgraefen <mgraefen@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/03 09:12:17 by mgraefen          #+#    #+#             */
+/*   Updated: 2023/02/03 09:12:26 by mgraefen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long	ft_atol(const char *str)
+{
+	int			i;
+	int			sign;
+	long		out;
+
+	i = 0;
+	out = 0;
+	sign = 1;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+	{
+		sign = (sign * -1);
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	if (str[i] >= '0' && str[i] <= '9')
+	{
+		while (str[i] >= '0' && str[i] <= '9')
+			out = ((out * 10) + (str[i++] - '0'));
+		return (out * sign);
+	}
+	return (0);
+}
